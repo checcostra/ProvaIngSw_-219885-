ea3bab85439aa5bb4c74fa8d9ec814f3f466d8d9
checco_stra <strangisfrancesco2@gmail.com>
2022-10-28 16:30:39 +0200

    implemented tests for the exceptions

--- src/test/java/org/example/Algorithmstest.java
+++ src/test/java/org/example/Algorithmstest.java
@@ -12,6 +12,11 @@ public class Algorithmstest {
     static String stringcheck3;
     static String stringcheck4;
     static String stringcheck5;
+    static String incorrectstringcheck;
+    static String incorrectstringcheck2;
+    static String incorrectstringcheck3;
+    static String incorrectstringcheck4;
+    static String incorrectstringcheck5;
 
     @BeforeClass
     public  static void inizializzazione(){
@@ -20,6 +25,11 @@ public class Algorithmstest {
         stringcheck3 = "-10";
         stringcheck4 = "32767";
         stringcheck5 = "-3";
+        incorrectstringcheck = "2 3";
+        incorrectstringcheck2 = "32768";
+        incorrectstringcheck3 = "A3";
+        incorrectstringcheck4 = "2.3";
+        incorrectstringcheck5 = "-32769";
     }
 
 
@@ -30,6 +40,19 @@ public class Algorithmstest {
         assertEquals(-10,f.stringToIntConverter(stringcheck3));
         assertEquals(32767,f.stringToIntConverter(stringcheck4));
         assertEquals(-3,f.stringToIntConverter(stringcheck5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ExceptionTest_IllegalArgumentException(){
+        f.stringToIntConverter(incorrectstringcheck2);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void ExceptionTest_NumberFormatException(){
+        f.stringToIntConverter(incorrectstringcheck);
+        f.stringToIntConverter(incorrectstringcheck3);
+        f.stringToIntConverter(incorrectstringcheck4);
+        f.stringToIntConverter(incorrectstringcheck5);
     }
 
 
